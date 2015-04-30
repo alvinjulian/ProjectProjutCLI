15c2e6739fb241557e4d49086a39433ab8b93799
images2907 <images2907@yahoo.co.id>
2015-04-30 11:41:02 +0700

    Update 1
    Mulai project baru, sebelumnya error

--- ProjectProjutCLI/Program.cs
+++ ProjectProjutCLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,68 +8,33 @@ using System.Threading.Tasks;
 namespace ProjectProjutCLI
 {
     class Program
-    {
-
-        public static Byte pilihan = 0;
-
-        static void Main(string[] args)
+    {   
+        static void Main()
         {
-            for (; ; )
+            printMenu();
+            Int16 pilih = 0;
+            Console.WriteLine("Masukan pilihan anda : ");
+            pilih = Convert.ToInt16(Console.ReadLine());
+            switch (pilih)
             {
-                Console.WriteLine("\t\t\t\tSistem Perpustakaan");
-                Console.WriteLine("\t\t\t\t===================");
-                Console.WriteLine("1. Menu Murid\n");
-                Console.WriteLine("2. Menu Buku\n");
-                Console.WriteLine("3. Menu Peminjaman\n");
-                Console.WriteLine("4. Exit Program\n");
-                Console.Write("Masukan pilihan anda : ");
-                pilihan = byte.Parse(Console.ReadLine());
-                switch (pilihan)
-                {
-                    case 1:
-                        buku.menuBuku();
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Perintah yang anda masukan salah!");
-                        break;
-                }
+                case 1:
+                    break;
+                case 2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
             }
         }
-    }
 
-
-    class buku
-    {
-        public static void menuBuku()
+        static void printMenu()
         {
-            for(;;)
-            {
-                Console.WriteLine("\t\t\t\tMenu Buku");
-                Console.WriteLine("\t\t\t\t=========");
-            }
-        }
-    }
-
-    class murid
-    {
-        public static void menuMurid()
-        {
-
-        }
-    }
-
-    class peminjaman
-    {
-        public static void menuPeminjaman()
-        {
-
+            Console.WriteLine("\t\t\t\tSistem Perpustakaan");
+            Console.WriteLine("\t\t\t\t===================");
+            Console.WriteLine("1. Menu Murid\n");
+            Console.WriteLine("2. Menu Buku\n");
+            Console.WriteLine("3. Menu Peminjaman\n");
+            Console.WriteLine("4. Exit Program\n");
         }
     }
 }
--- ProjectProjutCLI/Properties/AssemblyInfo.cs
+++ ProjectProjutCLI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fffb850b-eb1c-4bcc-b4f0-165f33aa323a")]
+[assembly: Guid("279673e4-812c-4b19-9dc7-8a87bc7fbd0b")]
 
 // Version information for an assembly consists of the following four values:
 //
