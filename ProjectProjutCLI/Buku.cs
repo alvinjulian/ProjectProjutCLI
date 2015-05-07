ccc61fc2e8e03760db9caf74834babf92778a1df
images2907 <images2907@yahoo.co.id>
2015-05-07 14:28:09 +0700

    Nambahin search judul buku (belum akurat)

--- ProjectProjutCLI/Buku.cs
+++ ProjectProjutCLI/Buku.cs
@@ -84,18 +84,24 @@ namespace ProjectProjutCLI
             Console.Clear();
             Console.WriteLine("\t\t\t\tList Buku berdasarkan judul");
             Console.WriteLine("\t\t\t\t===========================\n");
-            Console.Write("Masukkan kata-kata judul buku:");
+            
             do
             {
+                Console.Write("Masukkan kata-kata judul buku:");
                 keywordJudul = Console.ReadLine();
                 if(keywordJudul != "")
                 {
                     continue;
                 }
+                Console.WriteLine("Kata kunci judul tidak valid!..");
+                Console.WriteLine("Tekan sembarang untuk memasukan kata kunci....");
+                Console.ReadLine();
             }
             while (keywordJudul == "");
             //Coding untuk search berdasarkan judul
             lookJudul(keywordJudul);
+            Console.Write("Klik sembarang untuk kembali ke menu buku...");
+            Console.ReadLine();
             MainBuku();
         }
 
@@ -156,17 +162,48 @@ namespace ProjectProjutCLI
                 //Console.WriteLine(line);
                 counter++;
             }
+
+            Console.WriteLine("Total buku terdaftar : {0}", counter);
+
             sr.Close();
         }
 
-        static string lookJudul(string s)
+        static void lookJudul(string s)
         {
             string line;
-            string pattern;
+            int counter = 0;
 
-            pattern = s;
+            string pattern = @"\t+";
 
-            return s;
+            Regex rgx = new Regex(pattern);
+
+            string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = dir + @"\book.txt";
+            StreamReader sr = new StreamReader(file);
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] result = rgx.Split(line);
+
+                //string uji = result[0];
+
+                    if (Regex.IsMatch(result[0], s, RegexOptions.IgnoreCase))
+                    {
+                        Console.WriteLine("Nama buku\t:\t{0}", result[0]);
+                        Console.WriteLine("Pengarang buku\t:\t{0}", result[1]);
+                        Console.WriteLine("Edisi buku\t:\t{0}", result[2]);
+                        Console.WriteLine("Tanggal Kembali\t:\t{0}", result[3]);
+                        Console.WriteLine("NIM Peminjam\t:\t{0}", result[4]);
+                        Console.WriteLine();
+                        counter++;
+                    }
+                    //else
+                    //{
+                    //    continue;
+                    //}
+                //}
+            }
+            Console.WriteLine("Jumlah buku sesuai kata kunci : {0}", counter);
         }
     }
 }
