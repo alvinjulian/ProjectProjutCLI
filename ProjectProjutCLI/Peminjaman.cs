53dbfd608a29f4873241c37d9af5b543735f53db
Vallenteer <kristiantokelvin@yahoo.co.id>
2015-05-15 18:30:07 +0700

    Pinjam buku kondisi
    sudah dapat mendetek semua kondisi dalam peminjaman buku, tinggal edit
    ganti tanggal sma nim peminjam dari book.txt,,,
    book.txt ada w rubah ke format tanggal biar bner wkak

--- ProjectProjutCLI/Peminjaman.cs
+++ ProjectProjutCLI/Peminjaman.cs
@@ -70,41 +70,76 @@ namespace ProjectProjutCLI
         static void pinjamBuku()
         {
             int nim;
-            string judul;
-            string edisi;
+            string idbuku;
+            //string judul;
+            //string edisi;
             Console.Clear();
             Console.WriteLine("\t\t\t\tMenu Pinjam Buku");
             Console.WriteLine("\t\t\t\t================\n");
-            Console.Write("Masukan judul buku : ");
-            judul = Console.ReadLine();
-            Console.Write("Masukan edisi buku : ");
-            edisi = Console.ReadLine();
-
-            //coding cek buku apakah available
-            
-            //jika buku not available
-            Console.WriteLine("Buku tidak bisa dipinjam karena lagi dipinjam orang lain");
-            Console.WriteLine("Tekan sembarang untuk kembali ke menu...");
-            Console.Read();
-            MainPeminjaman();
-
-            //jika buku available
-            Console.Write("Masukan NIM mahasiswa : ");
-            nim = int.Parse(Console.ReadLine());
-                //jika nim tidak valid
-                Console.WriteLine("Tidak ada mahaiswa dengan NIM tersebut!");
-                Console.Write("Tekan sembarang untuk kembali ke menu...");
-                Console.Read();
+            Console.Write("Masukan id buku : ");
+            idbuku = Console.ReadLine();
+            //mengecek buku bisa dipinjam atau tidak
+            if (cekBuku(idbuku) == false)
+            {
+                Console.WriteLine("Apakah buku ini ingin dipinjam? Y/N ");
+                switch (char.ToUpper(Console.ReadKey().KeyChar))
+                {
+                    case 'Y':
+                        break;
+                    default :
+                        // selain Y dianggap N
+                        Console.WriteLine("\nTekan sembarang untuk kembali ke menu peminjaman");
+                        Console.ReadLine();
+                        MainPeminjaman();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tekan sembarang untuk kembali ke menu peminjaman");
+                Console.ReadLine();
                 MainPeminjaman();
 
-                //jika sudah meminjam 5 buku
-                Console.WriteLine("Maaf mahasiswa ini sudah meminjam 5 buku");
-                Console.Write("Tekan sembarang untuk kembali ke menu...");
+            }
+            Console.ReadLine();
+            //jika buku available       
+            do
+            {
+                Console.Write("\n\nMasukan NIM mahasiswa : ");
+                nim = int.Parse(Console.ReadLine());
+                //dirubah jadi bentuk int diantara 1 jt hinga 999999
+                if (nim >= 100000 && nim < 1000000)
+                {
+                    if (cekNIM2(nim) == true)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        //jika nim tidak valid atau sudah pinjam lebih dari 5 buku
+                        nim = 0;
+                    }
+
+                }
+                else
+                {
+                    //jika nim tidak valid
+                    Console.WriteLine("Tidak ada mahaiswa dengan NIM tersebut!\n");
+                    nim = 0;
+                }
+
+            } while (nim <= 100000 || nim >= 1000000);
+            //cek apakah dia ada telat mengembalikan buku atau tidak..
+            if(cektanggal(nim)==false)
+            {
+                Console.Write("\nTekan sembarang untuk kembali ke menu...");
                 Console.Read();
                 MainPeminjaman();
+            }
+                //masukin data peminjaman ke dalam file
 
-                //jika telat mengembalikan buku
-                Console.WriteLine("Maaf, mahasiswa ini ada buku yang sudah overdue belum dikembalikan");
+
+                Console.WriteLine("Peminjaman berhasil!!\n");
                 Console.Write("Tekan sembarang untuk kembali ke menu...");
                 Console.Read();
                 MainPeminjaman();
@@ -188,5 +223,121 @@ namespace ProjectProjutCLI
                 Console.WriteLine("Jumlah buku yang dipinjam : {0}", counter);
             }
         }
+
+        public static bool cekBuku(string id)
+        {
+            string line;
+            int counter = 0;
+            string pattern = @"\t+";
+            Regex rgx = new Regex(pattern);
+
+            string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = dir + @"\book.txt";
+            StreamReader sr = new StreamReader(file);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] result = rgx.Split(line);
+                //mengecek apakah idbuku benar
+                if (result[0] == id)
+                {
+                    counter++;
+                    //mengecek apakah ada yang pinjam
+                    if (result[4] != "-" && result[5] != "-")
+                    {
+                        Console.WriteLine("Buku sudah ada yang pinjam!\n");
+                        sr.Close();
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Judul Buku\t:\t{0}\n", result[1]);
+                        Console.WriteLine("Edisi Buku\t:\t{0}\n\n", result[3]);
+                    }
+                }
+            }
+            if (counter <= 0)
+            {
+                Console.WriteLine("Id Buku tidak terdaftar!\n");
+                sr.Close();
+                return true;
+            }
+            else
+            {
+                sr.Close();
+                return false;
+            }
+        }
+
+        public static bool cekNIM2(int nimcek)
+        {
+            string nim;
+            nim = nimcek.ToString();
+            string line;
+            string pattern = @"\t+";
+            Regex rgx = new Regex(pattern);
+            string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = dir + @"\student.txt";
+            StreamReader sr = new StreamReader(file);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] result = rgx.Split(line);
+                //membuat baris list murid
+                if (result[0] == nim)
+                {
+                    sr.Close();
+                    return true;
+                }
+
+            }
+            //kalau nim tak terdaftar
+            Console.WriteLine("Tidak ada mahaiswa dengan NIM tersebut!\n");
+            sr.Close();
+            return false;
+        }
+        public static bool cektanggal(int nimcek)
+        {
+            string nim;
+            nim = nimcek.ToString();
+            
+            string line;
+            int counter = 0;
+            string pattern = @"\t+";
+            Regex rgx = new Regex(pattern);
+
+            string dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string file = dir + @"\book.txt";
+            StreamReader sr = new StreamReader(file);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] result = rgx.Split(line);
+                //mengecek apakah nim pernah pinjam buku atau tidak
+                if (result[5] == nim)
+                {
+                    counter++;
+                    DateTime duedate= Convert.ToDateTime(result[4]);
+                    DateTime today= DateTime.Today;
+                    if ((duedate - today).TotalDays < 0)
+                    {
+                        //kalau lewad due
+                        Console.WriteLine("Maaf, mahasiswa ini ada buku yang sudah overdue belum dikembalikan");
+                        sr.Close();
+                        return false;
+                    }
+                }
+            }
+            if (counter >= 5)
+            {
+                //kalau pinjam lebih dari 5 buku
+
+                Console.WriteLine("Maaf mahasiswa ini sudah meminjam 5 buku");
+                sr.Close();
+                return false;
+            }
+            else
+            {
+                sr.Close();
+                return true;
+            }
+        }        
     }
 }
